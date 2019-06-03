a6b5e680ee47dae75e600774b84db35838c6f159
Max Misrahi <maxmisrahi@Maxs-Air.lan>
2019-06-03 11:45:22 +0100

    Initial Changes

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const app = express();
+
+var properties = new Array();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req,res) => {
+    console.log (req.headers);
+    res.send("<p>Hello</p>");
+});
+
+app.get("/properties", (req, res) => {
+    // var properties = new Array();
+    // properties.push({
+    //     id: 1,
+    //     name: "One", 
+    //     locations: "Lisbon"
+    // });
+    res.json(properties);
+});
+
+app.post("/properties", (req, res) => {
+    const property = req.body;
+    console.log(property);
+    res.json(property);
+});
+
+app.listen(3000, (err) => {
+    if (err) {
+    console.log("Oops, error");
+    return;
+}   
+
+    console.log ("Server listening port 3000");
+})
+
+console.log("This app runs! Yay");
